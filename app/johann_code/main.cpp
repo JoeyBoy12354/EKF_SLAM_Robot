500b20371913e6cded077812831ca9e91e34513b
Johann Strydom <u20594055@tuks.co.za>
2023-09-09 19:43:29 +0200

    fix

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -164,7 +164,7 @@ void testRun(){
     bool mapped = false;
     bool firstRun = true;
     
-    for(int i =0;i<3;i++){
+    for(int i =0;i<2;i++){
         fullRun(ekf,mapped,firstRun);
     }
     
