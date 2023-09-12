5739c9a372a90a6c0d9f99722ee29f9c331850be
Johann Strydom <u20594055@tuks.co.za>
2023-09-12 15:42:22 +0200

    dropped to 2 runs

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -208,7 +208,7 @@ void testRun(){
     bool mapped = false;
     bool firstRun = true;
     
-    for(int i =0;i<5;i++){
+    for(int i =0;i<2;i++){
         cout<<"IN RUN LOOP: "<<i<<endl;
         cout<<"Mapped = "<<mapped<<endl;
         fullRunfullLandmark(ekf,mapped,firstRun);
