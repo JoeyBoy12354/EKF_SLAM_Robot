b738ba531b02f9dd386f5228267dbb3e6944ed8a
Johann Strydom <u20594055@tuks.co.za>
2023-09-20 23:30:33 +0200

    slow

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -141,13 +141,13 @@ def forward_slow(distance):
     NoRotations = distance/(2*PI*r)
     W12_Ticks = NoRotations*20
 
-    for i in range(0,10):
+    for i in range(0,100):
         wiringpi.digitalWrite(LMot_Pin, 0)  # Write 1 ( HIGH ) to pin 6
         wiringpi.digitalWrite(RMot_Pin, 0)  # Write 1 ( HIGH ) to pin 7
-        time.sleep(0.1)
+        time.sleep(0.01)
         wiringpi.digitalWrite(LMot_Pin, 1)  # Write 0 ( LOW ) to pin 6
         wiringpi.digitalWrite(RMot_Pin, 1)  # Write 0 ( LOW ) to pin 7
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 
 
