2920f96aae696df3a09faf00b12eca97f77edbef
Johann Strydom <u20594055@tuks.co.za>
2023-09-22 19:43:43 +0200

    test swivel

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -783,29 +783,29 @@ def testThread(distance):
       
 def testSpeedControl(angle,distance):
     wait = 5
-    print("Forward for ",distance)
-    speedControl(0,distance,True)
-    # print("Reverse for ",distance)
-    # speedControl(0,distance,False)
+    # print("Forward for ",distance)
+    # speedControl(0,distance,True)
+    # # print("Reverse for ",distance)
+    # # speedControl(0,distance,False)
 
-    time.sleep(wait)
+    # time.sleep(wait)
 
     print("Left for ",angle*180/PI)
     speedControl(angle,0,True)
     time.sleep(wait)
-    print("Left_R for ",angle*180/PI)
-    speedControl(angle,0,False)
-    time.sleep(wait)
+    # print("Left_R for ",angle*180/PI)
+    # speedControl(angle,0,False)
+    # time.sleep(wait)
 
-    print("Right for ",angle*180/PI)
-    speedControl(-1*angle,0,True)
-    print("Right_R for ",angle*180/PI)
-    speedControl(-1*angle,0,False)
-    time.sleep(wait)
+    # print("Right for ",angle*180/PI)
+    # speedControl(-1*angle,0,True)
+    # print("Right_R for ",angle*180/PI)
+    # speedControl(-1*angle,0,False)
+    # time.sleep(wait)
 
-    print("Forward for ",distance)
-    speedControl(0,distance,True)
-    time.sleep(wait)
+    # print("Forward for ",distance)
+    # speedControl(0,distance,True)
+    # time.sleep(wait)
 
 
     
