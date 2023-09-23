88d4ae5b4e2753cb27329b841d71632d1d1f1b21
Johann Strydom <u20594055@tuks.co.za>
2023-09-23 19:33:47 +0200

    removed arduino

--- output/Linux/Release/gyro.py
+++ output/Linux/Release/gyro.py
@@ -1,40 +1,31 @@
-import smbus
+import odroid_wiringpi as wiringpi
 import time
 
-# Define the I2C bus (1 for Odroid XU4)
-bus = smbus.SMBus(1)
+# Initialize the I2C interface
+wiringpi.wiringPiSetup()
 
-# LSM6DSOX I2C address
-lsm6dsox_address = 0x6A  # Check the datasheet for the correct address
+# Define the I2C address of the LSM6DSOX sensor
+lsm6dsox_address = 0x6A  # Change this to your sensor's address if different
 
 # Configure the sensor (you may need to adjust these settings)
-bus.write_byte_data(lsm6dsox_address, 0x10, 0x40)  # CTRL1_XL register, set accelerometer to 2g
-bus.write_byte_data(lsm6dsox_address, 0x11, 0x40)  # CTRL2_G register, set gyroscope to 2000 dps
+# Example: Set accelerometer to 2g range and gyroscope to 2000 dps
+wiringpi.wiringPiI2CWriteReg8(lsm6dsox_address, 0x10, 0x40)  # CTRL1_XL register
+wiringpi.wiringPiI2CWriteReg8(lsm6dsox_address, 0x11, 0x40)  # CTRL2_G register
 
 # Read accelerometer and gyroscope data
 while True:
     # Read accelerometer data (16-bit signed values)
-    accel_x = bus.read_i2c_block_data(lsm6dsox_address, 0x28, 2)
-    accel_y = bus.read_i2c_block_data(lsm6dsox_address, 0x2A, 2)
-    accel_z = bus.read_i2c_block_data(lsm6dsox_address, 0x2C, 2)
-    
-    # Read gyroscope data (16-bit signed values)
-    gyro_x = bus.read_i2c_block_data(lsm6dsox_address, 0x22, 2)
-    gyro_y = bus.read_i2c_block_data(lsm6dsox_address, 0x24, 2)
-    gyro_z = bus.read_i2c_block_data(lsm6dsox_address, 0x26, 2)
-    
-    # Convert raw data to values (you may need to adjust the scaling factors)
-    accel_x = (accel_x[1] << 8 | accel_x[0]) / 16384.0  # 2g range
-    accel_y = (accel_y[1] << 8 | accel_y[0]) / 16384.0  # 2g range
-    accel_z = (accel_z[1] << 8 | accel_z[0]) / 16384.0  # 2g range
-    
-    gyro_x = (gyro_x[1] << 8 | gyro_x[0]) / 16.4  # 2000 dps range
-    gyro_y = (gyro_y[1] << 8 | gyro_y[0]) / 16.4  # 2000 dps range
-    gyro_z = (gyro_z[1] << 8 | gyro_z[0]) / 16.4  # 2000 dps range
-    
-    # Print the sensor data
-    print("Accelerometer (g): X={:.2f}, Y={:.2f}, Z={:.2f}".format(accel_x, accel_y, accel_z))
-    print("Gyroscope (dps): X={:.2f}, Y={:.2f}, Z={:.2f}".format(gyro_x, gyro_y, gyro_z))
+    accel_x = wiringpi.wiringPiI2CReadReg16(lsm6dsox_address, 0x28)
+    accel_y = wiringpi.wiringPiI2CReadReg16(lsm6dsox_address, 0x2A)
+    accel_z = wiringpi.wiringPiI2CReadReg16(lsm6dsox_address, 0x2C)
 
-    
+    # Read gyroscope data (16-bit signed values)
+    gyro_x = wiringpi.wiringPiI2CReadReg16(lsm6dsox_address, 0x22)
+    gyro_y = wiringpi.wiringPiI2CReadReg16(lsm6dsox_address, 0x24)
+    gyro_z = wiringpi.wiringPiI2CReadReg16(lsm6dsox_address, 0x26)
+
+    # Print the sensor data
+    print(f"Accelerometer (g): X={accel_x:.2f}, Y={accel_y:.2f}, Z={accel_z:.2f}")
+    print(f"Gyroscope (dps): X={gyro_x:.2f}, Y={gyro_y:.2f}, Z={gyro_z:.2f}")
+
     time.sleep(1)  # Wait for 1 second before reading again
