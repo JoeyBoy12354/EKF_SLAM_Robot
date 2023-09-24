3126491124c62b99a88eec116aaf0d3d23f49681
Johann Strydom <u20594055@tuks.co.za>
2023-09-24 20:16:16 +0200

    more prints

--- app/johann_code/Data_Functions.cpp
+++ app/johann_code/Data_Functions.cpp
@@ -125,7 +125,7 @@ namespace Data_Functions{
 
         
         readMotorFromCSV(theta,dist);
-        cout<<"Read angle = "<<theta<<" Read Distance = "<<endl;
+        cout<<"SEND TO EKF: Read angle = "<<theta<<" Read Distance = "<<endl;
         
         ekf_w = theta;
         ekf_dist = dist;
