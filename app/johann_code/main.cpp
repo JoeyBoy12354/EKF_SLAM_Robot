2cd91b860b6fcfb3010daef0eef78478cbadf90f
Johann Strydom <u20594055@tuks.co.za>
2023-09-28 15:17:49 +0200

    input grid dots

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -138,8 +138,8 @@ void fullRun(ExtendedKalmanFilter& ekf,bool& mapped, bool& firstRun){
         lidarDataProcessing(lidarDataPoints,carPoints,ekf.State[0],ekf.State[1],ekf.State[2]);
 
         //Get Grid
-        // vector<vector<GridPoint>> gridNew;
-        // gridDataProcess(carPoints, gridNew, ekf.State, firstRun);
+        vector<vector<GridPoint>> gridNew;
+        gridDataProcess(carPoints, gridNew, ekf.State, firstRun);
 
         //Run EKF
         ekf.runEKF();
