8dd8074ab3d243a6b1ae783a6ed2e95ee645a027
Johann Strydom <u20594055@tuks.co.za>
2023-09-29 13:24:16 +0200

    fix

--- output/Linux/Release/livePlotter.py
+++ output/Linux/Release/livePlotter.py
@@ -171,6 +171,10 @@ def animate(i):
     plt.cla() #Clear Axis (so that we don't keep the old plot we clear it and write again)
     #plt.axes().set_facecolor("black")
 
+    plt.axhline(y=0, color='k', linestyle='--', linewidth=1)
+    plt.axvline(x=0, color='k', linestyle='--', linewidth=1)
+    fetchAndPlotGrid()
+
     #fetchAndPlotLines()
     triangle = draw_rotated_triangle(plt.gca(),position[0],position[1],position[2])
     plt.gca().fill(triangle[:, 0], triangle[:, 1], 'b')
@@ -179,9 +183,8 @@ def animate(i):
     #triangle = draw_rotated_triangle(plt.gca(),true_move[0],true_move[1],true_move[2])
     #plt.gca().fill(triangle[:, 0], triangle[:, 1], 'y')
 
-    fetchAndPlotGrid()
-    plt.axhline(y=0, color='k', linestyle='--', linewidth=1)
-    plt.axvline(x=0, color='k', linestyle='--', linewidth=1)
+    
+    
     plt.plot(x1, y1, 'o', label='Points',markersize=0.5,color='r')
     plt.plot(x4, y4, 'o', label='Con_points', markersize=1,color='purple')
     plt.plot(x2, y2, 'X', label='Landmarks', markersize=7,color='g')
