41dc44aff32bb95d3efb0a9d0355d77922bc7419
Johann Strydom <u20594055@tuks.co.za>
2023-09-30 18:14:31 +0200

    fix

--- app/johann_code/Mapping_Functions.cpp
+++ app/johann_code/Mapping_Functions.cpp
@@ -227,7 +227,7 @@ namespace Mapping_Functions{
     void gridMakeDots(vector<CarPoint> mapdata, vector<vector<GridPoint>>& points){
         
         for(int i=0;i<mapdata.size();i++){
-            if(mapdata[i].x < 0 && mapdata[i].y<-300){
+            if(mapdata[i].x>-200 && mapdata[i].x < -150 && mapdata[i].y<-400){
                 cout<<"GRID mapdata:"<<mapdata[i].x<<","<<mapdata[i].y<<endl;
             }
         }
@@ -252,6 +252,9 @@ namespace Mapping_Functions{
 
         vector<float> bounds; //Xmax,Xmin,Ymax,Ymin
         getMapBounds(mapdata,bounds);
+        if(mapdata[i].x>-200 && mapdata[i].x < -150 && mapdata[i].y<-400){
+                cout<<"GRID mapdata:"<<mapdata[i].x<<","<<mapdata[i].y<<endl;
+        }
 
         vector<GridPoint> yPoints;
         GridPoint newPoint;
