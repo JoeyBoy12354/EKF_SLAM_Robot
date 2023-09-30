32d1f97555f4d31a631d619e7ff0ce9d264f5cc0
Johann Strydom <u20594055@tuks.co.za>
2023-09-30 12:14:46 +0200

    RANSAC Tol 20_.30

--- app/johann_code/Landmark_Functions.cpp
+++ app/johann_code/Landmark_Functions.cpp
@@ -229,7 +229,7 @@ namespace Landmark_Functions{
         //const int RANSAC_CONSENSUS = 30; //RANSAC: at least X points in consensus required to determine if a line will be deemed valid 
 
         //As of right now 24 August we are in mm and doing about 10000 samples
-        const double RANSAC_TOLERANCE = 20; //RANSAC: if point is within x distance of line its part of line [mm]
+        const double RANSAC_TOLERANCE = 30; //RANSAC: if point is within x distance of line its part of line [mm]
         const int RANSAC_CONSENSUS = 1000; //RANSAC: at least X points in consensus required to determine if a line will be deemed valid 
         const int MINLINEPOINTS = 100;//If less than 100 points left don't bother trying to find consensous
 
