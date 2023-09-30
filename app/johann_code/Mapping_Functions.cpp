16fecde444e5d6ed21687eac12d6541c151bd9fb
Johann Strydom <u20594055@tuks.co.za>
2023-09-30 17:10:08 +0200

    fix

--- app/johann_code/Mapping_Functions.cpp
+++ app/johann_code/Mapping_Functions.cpp
@@ -209,7 +209,7 @@ namespace Mapping_Functions{
             float temp_dist = pointDistance(searchMap[i],point2);
 
             if(point.x == -200 && point.y== -400 && searchMap[i].y < -200 && searchMap[i].x < -150){
-                cout<<"GRID dist = "<<temp_dist<<" PNT:"<<point.x<<","<<point.y<<" SM:"<<searchMap.x<<","<<searchMap.y<<endl;
+                cout<<"GRID dist = "<<temp_dist<<" PNT:"<<point.x<<","<<point.y<<" SM:"<<searchMap[i].x<<","<<searchMap[i].y<<endl;
             }
 
             if(temp_dist <= distThresh){
