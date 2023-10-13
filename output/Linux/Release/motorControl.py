9f9ba1af527fb99af6611daa9b9062572719a9a3
Johann Strydom <u20594055@tuks.co.za>
2023-10-13 14:27:11 +0200

    test

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -654,9 +654,9 @@ wiringpi.digitalWrite(LMot_Pin, 1)
 #test3()
 
 angle,distance = readInstructions()
-#angle = PI/2
-angle = 0
-distance = 300
+tangle = PI/2
+#angle = 0
+#distance = 300
 
 print("MC: TEST SET angle = ",angle," distance = ",distance)
 
