1729df9dd8ed420eb70c30ead7a3469c296cb4c1
Johann Strydom <u20594055@tuks.co.za>
2023-10-16 22:27:22 +0200

    fix

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -40,7 +40,7 @@ ExtendedKalmanFilter::ExtendedKalmanFilter() {
     //Covariance.setIdentity();
 
     //Use initialLandmarkCovariance_AtSi;
-    float test = 1;
+    float test = 10;
     Covariance.setZero();
     for (int n = 3; n < dim; n++) {
         Covariance(n, n) = test;
