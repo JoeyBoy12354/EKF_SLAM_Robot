9e5f77e313c4bb47d58dceff1927f74db240bf44
Johann Strydom <u20594055@tuks.co.za>
2023-10-16 21:08:34 +0200

    fix

--- app/johann_code/robot.h
+++ app/johann_code/robot.h
@@ -248,7 +248,7 @@ public:
     // float w = -0.12391118;
     // float t = 0.1;
 
-    float sigma_r = 5;//*
+    float sigma_r = 8;//*
     //float sigma_theta = 0.17;//* 
     float sigma_theta = 0.06;//* 
     float sigma_odo_x = 0.01;
