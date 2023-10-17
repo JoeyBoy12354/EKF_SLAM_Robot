f97d6cf903d4b7180d71112d2d7ba338532ce2a9
Johann Strydom <u20594055@tuks.co.za>
2023-10-17 15:51:53 +0200

    fix

--- app/johann_code/Navigation_Functions.cpp
+++ app/johann_code/Navigation_Functions.cpp
@@ -296,7 +296,7 @@ namespace Navigation_Functions{
         angle = PI-angle;
 
         //angle = PI/2;
-        //angle = 30*PI/180;
+        angle = 0;
 
         // cout<<"NAVI,GRID: deltaX = "<<deltaX<<" = "<<-1*closestPoint.x<<" - "<<robotPoint.x<<endl;
         // cout<<"NAVI,GRID: deltaY = "<<deltaY<<" = "<<-1*closestPoint.y<<" - "<<robotPoint.y<<endl;
