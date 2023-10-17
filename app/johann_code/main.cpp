3e8150e1bd71283e91b508ab0ada6720497e6f85
Johann Strydom <u20594055@tuks.co.za>
2023-10-17 14:15:38 +0200

    fix

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -654,7 +654,7 @@ void testRun(){
     }
 
     finalRun = 1;
-    for(int i =0;i<20;i++){
+    for(int i =0;i<100;i++){
         cout<<"\n i = "<<"FINALRUN "<<i<<endl;
         cout<<"------------------------------------------------------------------------------------------------------------\n\n";
         fullRun(ekf,mapped,firstRun,finalRun);
