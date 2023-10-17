600e06d13d9d651d1570b7bf65b55ba861d68d9f
Johann Strydom <u20594055@tuks.co.za>
2023-10-17 23:04:37 +0200

    lko

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -395,7 +395,7 @@ void ExtendedKalmanFilter::updateStateOfLandmark() {
     
     Gain2(1) = -Gain(1);
     if(delta_z(1)*Gain2[2]<0 ){
-        //Gain2(2) = -Gain(2);
+        Gain2(2) = -Gain(2);
         cout<<"angle update opposite"<<endl;
     }
     if((ObservedLandmark.x-EstimatedLandmark.x)*Gain2[0] < 0){
