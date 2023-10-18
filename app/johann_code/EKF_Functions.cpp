5cfcb76598dff51093c40dfb6cf481131df0f84a
Johann Strydom <u20594055@tuks.co.za>
2023-10-18 10:14:57 +0200

    ag

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -282,8 +282,8 @@ void ExtendedKalmanFilter::getEstimatedObservationJacobian(float deltaX, float d
     // Observation_Jacobian_low << sq*deltaX, sq*deltaY,    0, sq*deltaX,  sq*deltaY, 
     //                             deltaY,       -1*deltaX,    -1*q, -1*deltaY,  deltaX;
 
-    Observation_Jacobian_low << -1*sq*deltaX, -1*sq*deltaY,    0, sq*deltaX,  sq*deltaY, 
-                                deltaY,       -1*deltaX,    q, -1*deltaY,  deltaX;
+    // Observation_Jacobian_low << -1*sq*deltaX, -1*sq*deltaY,    0, sq*deltaX,  sq*deltaY, 
+    //                             deltaY,       -1*deltaX,    q, -1*deltaY,  deltaX;
 
     Observation_Jacobian_low << -1*sq*deltaX, sq*deltaY,    0, sq*deltaX,  -1*sq*deltaY, 
                                 deltaY,       deltaX,    -1*q, -1*deltaY,  -1*deltaX;
