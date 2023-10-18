6e334daf5fd66cf0667c26ea4a2fc3d9abe41529
Johann Strydom <u20594055@tuks.co.za>
2023-10-18 11:53:38 +0200

    ya

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -364,7 +364,7 @@ void ExtendedKalmanFilter::updateStateOfLandmark() {
     cout<<"\nGain = "<<endl;
     for(int i =0;i<dim;i++){
         if(Gain(i,0) != 0 && Gain(i,0) != 0){
-            cout<<Gain(i,0)<<" | "<<Gain(i,0)<<endl;
+            cout<<Gain(i,0)<<" | "<<Gain(i,1)<<endl;
         }
     }
 
