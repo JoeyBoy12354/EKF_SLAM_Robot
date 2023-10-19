a3d4533d2633d347dec581f2bb8c8c6b61bb2365
Johann Strydom <u20594055@tuks.co.za>
2023-10-19 21:53:23 +0200

    fix

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -389,7 +389,6 @@ void ExtendedKalmanFilter::updateStateOfLandmark() {
 
 
     //cout<<"(z-z_cap).r = "<<(z-z_cap)(0)<<"(z-z_cap).theta = "<<(z-z_cap)(1)*180/PI<<endl;
-    cout<<"b4_z = \n"<<z<<endl;
     
 
     Matrix<float, 2, 1> delta_z = z-z_cap;
@@ -461,7 +460,6 @@ void ExtendedKalmanFilter::runEKF() {
 
 
         isNewLandmark();
-        cout<<"post is new LM z.r = "<<z(0)<<"z.theta = "<<z(1)*180/PI<<endl;
 
         float deltaX = EstimatedLandmark.x - State(0);
         float deltaY = EstimatedLandmark.y - State(1);
@@ -474,8 +472,8 @@ void ExtendedKalmanFilter::runEKF() {
         //cout<<"ObsLM.x = "<<ObservedLandmark.x<<"ObsLM.y = "<<ObservedLandmark.y<<endl;
         // cout<<"EstLM.x = "<<EstimatedLandmark.x<<"EstLM.y = "<<EstimatedLandmark.y<<endl;
         // cout<<"deltaX = "<<deltaX<<" deltaY = "<<deltaY<<" q = "<<q<<endl;
-        cout<<"z.r = "<<z(0)<<"z.theta = "<<z(1)*180/PI<<endl;
-        cout<<"z_cap.r = "<<z_cap(0)<<"z_cap.theta = "<<z_cap(1)*180/PI<<endl;
+        // cout<<"z.r = "<<z(0)<<"z.theta = "<<z(1)*180/PI<<endl;
+        // cout<<"z_cap.r = "<<z_cap(0)<<"z_cap.theta = "<<z_cap(1)*180/PI<<endl;
 
         //getEstimatedObservation(deltaX, deltaY, q);
 
