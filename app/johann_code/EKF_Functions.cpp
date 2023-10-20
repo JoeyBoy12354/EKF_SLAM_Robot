14a547ebea26d602f8855cad8d66d9043096b9b9
Johann Strydom <u20594055@tuks.co.za>
2023-10-20 20:37:26 +0200

    fix

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -258,7 +258,9 @@ void ExtendedKalmanFilter::isNewLandmark2(){
     double smallestDistance = *min_element(minDistances.begin(), minDistances.end());
     cout<<"hello2"<<endl;
     int smallestDistanceIndex = indexes[getIndex(minDistances,smallestDistance)];
+    cout<<"minDistance.size() = "<<minDistances.size();
     cout<<"Smallest Distance = "<<smallestDistance<<endl;
+    cout<<"sas"<<endl;
     cout<<"Smallest Distance Index = "<<smallestDistanceIndex<<endl;
 
     if(smallestDistanceIndex == NoLandmarksFound){
