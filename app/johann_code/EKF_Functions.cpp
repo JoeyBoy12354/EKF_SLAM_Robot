9743e6d7f3a295dd4804944673d5fef47a3b3cc8
Johann Strydom <u20594055@tuks.co.za>
2023-10-20 22:41:16 +0200

    f

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -206,7 +206,7 @@ float ExtendedKalmanFilter::Mahalanobis_distance(CarPoint StoredPoint,int LMinde
     float deltaY = StoredPoint.y - State(1);
     //double q = deltaX * deltaX + deltaY * deltaY;
     double q = pow(deltaX,2) + pow(deltaY,2);
-    cout<<"q = "<<q<<", sqrt(q) = "<<sqrt(q);
+    cout<<"q = "<<q<<", sqrt(q) = "<<sqrt(q)<<"   ";
     z_cap_m(0) = sqrt(q);
     cout<<"z_q = "<<z_cap_m<<endl;
     z_cap_m(1) = (atan2(deltaY, deltaX)) - State(2);
@@ -215,7 +215,7 @@ float ExtendedKalmanFilter::Mahalanobis_distance(CarPoint StoredPoint,int LMinde
     z_cap_m(1) = pi_2_pi(z_cap_m(1));
 
     cout<<"DeltaX = "<<deltaX<<" deltaY = "<<deltaY<<endl;
-    cout<<"z_cap = "<<z_cap_m(0)<<", "<<z_cap_m(1);
+    cout<<"z_cap = "<<z_cap_m(0)<<", "<<z_cap_m(1)<<endl;
     Matrix<float, 2, 1> delta_z = z-z_cap_m;
     delta_z(1) = pi_2_pi(delta_z(1));
     cout<<"Delta_z = \n"<<delta_z<<endl;
