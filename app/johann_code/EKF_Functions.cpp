4a7371b96f5c345d505705f439f31676a22d9a3e
Johann Strydom <u20594055@tuks.co.za>
2023-10-21 15:47:56 +0200

    fix

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -269,7 +269,7 @@ float ExtendedKalmanFilter::Mahalanobis_distance(CarPoint StoredPoint,int LMinde
 
 
 void ExtendedKalmanFilter::isNewLandmark2(){
-    double distThresh = 1500;
+    double distThresh = 5000;
     //cout<<"In NewLandmark2"<<endl;
 
     vector<double> minDistances;
