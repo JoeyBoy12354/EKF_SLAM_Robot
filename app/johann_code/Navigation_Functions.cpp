a81b65f8f1dfd9543cbe85b18f4d9dc93a3aeb0d
Johann Strydom <u20594055@tuks.co.za>
2023-10-22 17:56:29 +0200

    fix

--- app/johann_code/Navigation_Functions.cpp
+++ app/johann_code/Navigation_Functions.cpp
@@ -344,7 +344,6 @@ namespace Navigation_Functions{
     vector<CarPoint> pathFinder(MatrixXf State,CarPoint Goal){
 
         cout<<"A1"<<endl;
-        cout<<"Input Goal = "<<Goal<<endl
         GridPoint myRobot;
         myRobot.x=State(0);
         myRobot.y=State(1);
