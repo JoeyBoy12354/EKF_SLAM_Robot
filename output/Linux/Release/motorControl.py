664e73fbda2d02a61a6af3b8737cffd2e311196a
Johann Strydom <u20594055@tuks.co.za>
2023-10-22 17:34:56 +0200

    fix

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -671,21 +671,20 @@ wiringpi.digitalWrite(LMot_Pin, 1)
 
 
 print("MC started")
-testSonar()
 
-# if(readState() == True):
-#     motorCalibrate()
-# else:
-#     angle,distance = readInstructions()
-#     timeOnL, timeOnR, timeOffL, timeOffR = readCalibration()
-#     # print("MC: time Left = ",timeOnL,"s ",timeOffL,"s")
-#     # print("MC: time Right = ",timeOnR,"s ",timeOffR,"s")
+if(readState() == True):
+    motorCalibrate()
+else:
+    angle,distance = readInstructions()
+    timeOnL, timeOnR, timeOffL, timeOffR = readCalibration()
+    # print("MC: time Left = ",timeOnL,"s ",timeOffL,"s")
+    # print("MC: time Right = ",timeOnR,"s ",timeOffR,"s")
 
 
-#     angle,distance = motorControl_wThread(angle,distance)
-#     print("MC: Angle turned = ",angle*180/math.pi)
-#     print("MC: distance moved = ",distance)
-#     writeOdometry(angle,distance)
+    angle,distance = motorControl_wThread(angle,distance)
+    print("MC: Angle turned = ",angle*180/math.pi)
+    print("MC: distance moved = ",distance)
+    writeOdometry(angle,distance)
 
 
 
