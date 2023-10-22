200ba12decfb488604cfa432e49c3d937fc1fc24
Johann Strydom <u20594055@tuks.co.za>
2023-10-22 18:05:28 +0200

    fix

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -290,7 +290,7 @@ float ExtendedKalmanFilter::directDistance(CarPoint StoredPoint){
 
 void ExtendedKalmanFilter::isNewLandmark2(){
     //double distThresh = 1000;
-    double distThresh = 600;
+    double distThresh = 270;//Was 600
     //cout<<"In NewLandmark2"<<endl;
 
     vector<double> minDistances;
