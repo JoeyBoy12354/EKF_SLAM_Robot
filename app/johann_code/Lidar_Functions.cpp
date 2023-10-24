d94b3e024c85f406fc8f4b9b14e6e68f0529ec56
Johann Strydom <u20594055@tuks.co.za>
2023-10-24 18:32:27 +0200

    as

--- app/johann_code/Lidar_Functions.cpp
+++ app/johann_code/Lidar_Functions.cpp
@@ -456,7 +456,7 @@ namespace Lidar_Functions{
             cout<<"startFlag = "<<startFlag<<endl;
             if(startFlag == false){
                 lidarDataPoints.clear();
-                cout<<"CLEARED"<<endl;
+                //cout<<"CLEARED"<<endl;
                 fetchScan(drv, op_result, lidarDataPoints, NoPoints, error, timeout);
 
 
