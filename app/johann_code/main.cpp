82e3cd5b7c24c8a05b1c333401d68f86d8028df3
Johann Strydom <u20594055@tuks.co.za>
2023-11-01 11:31:10 +0200

    fix

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -843,7 +843,7 @@ void testRun(){
     initializeLidar(drv,error);
 
     int count = 0;
-    while(mapped == false && count<3){
+    while(mapped == false && count<13){
         
         fetchScan(drv, op_result, lidarDataPoints, NoPoints, error, timeout);
         lidarDataPoints.clear();
