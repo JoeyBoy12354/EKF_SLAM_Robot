b45dd95b42302801dc52de3afdd8c20253c24470
Johann Strydom <u20594055@tuks.co.za>
2023-11-02 19:26:12 +0200

    fdsgd

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -972,7 +972,7 @@ void testRANSAC(){
     }
 
     cout<<"B x size = "<<x.size()<<endl;
-    int sample_size = 31;
+    int sample_size = 32;
     int max_iters= 200;
     float inlier_thresh=0.0006; //0.0005
     int min_inlier = 5; // 5
