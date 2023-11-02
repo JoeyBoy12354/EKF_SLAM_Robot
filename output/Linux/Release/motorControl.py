a1603a5ccb110a35d04263ebb3499c537d494290
Johann Strydom <u20594055@tuks.co.za>
2023-11-03 00:15:07 +0200

    fix

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -441,8 +441,8 @@ def motorCalibrate():
 
     print("MCAL: IN motor calibration")
     #default times
-    timeOn = 0.008
-    timeOff = 0.002
+    timeOn = 0.007
+    timeOff = 0.003
     timeOnL = timeOn
     timeOnR = timeOn
     timeOffL = timeOff
