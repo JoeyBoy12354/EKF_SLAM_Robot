e1ada4bebae021351474ccc8ade2ae99a315d4dc
Johann Strydom <u20594055@tuks.co.za>
2023-11-02 19:21:53 +0200

    fuxk

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -972,9 +972,9 @@ void testRANSAC(){
     }
 
     cout<<"B x size = "<<x.size()<<endl;
-    int sample_size = 30;
+    int sample_size = 35;
     int max_iters= 200;
-    float inlier_thresh=0.00065; //0.0005
+    float inlier_thresh=0.0006; //0.0005
     int min_inlier = 5; // 5
     float angleThreshold = 20.0 * M_PI / 180.0;
     float distanceThreshold = 120;
