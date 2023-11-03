5c8cec4066376944383da89d5a6f9ef324eea681
Johann Strydom <u20594055@tuks.co.za>
2023-11-03 14:28:00 +0200

    fix

--- app/johann_code/EKF_Functions.cpp
+++ app/johann_code/EKF_Functions.cpp
@@ -37,8 +37,8 @@ ExtendedKalmanFilter::ExtendedKalmanFilter() {
     float test_sigma_r = 0.5;
     float test_sigma_theta = 0.5;
     
-    float test_sigma_odo_x = 0.8;
-    float test_sigma_odo_y = 0.8;
+    float test_sigma_odo_x = 2;
+    float test_sigma_odo_y = 2;
     float test_sigma_odo_theta = 30*PI/180;
 
 
