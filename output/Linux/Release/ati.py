ec6743c96e48bc8f3cb2cfcbb74907ec44c27c4c
Johann Strydom <u20594055@tuks.co.za>
2023-11-03 10:49:30 +0200

    fix

--- output/Linux/Release/ati.py
+++ output/Linux/Release/ati.py
@@ -120,7 +120,9 @@ def observation(xTrue, xd, u, RFID):
         d = math.hypot(dx, dy)
         angle = pi_2_pi(math.atan2(dy, dx) - xTrue[2, 0])
         if d <= MAX_RANGE:
+            #print("dn = d + noise = ",dn," = ",d," + ",np.random.randn() * Q_sim[0, 0] ** 0.5)
             dn = d + np.random.randn() * Q_sim[0, 0] ** 0.5  # add noise
+            print("dn = d + noise = ",dn," = ",d," + ",np.random.randn() * Q_sim[0, 0] ** 0.5)
             angle_n = angle + np.random.randn() * Q_sim[1, 1] ** 0.5  # add noise
             zi = np.array([dn, angle_n, i])
             z = np.vstack((z, zi))
