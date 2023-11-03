e1de840d6ad0876e9b5a250620e2cf6989bd1a5d
Johann Strydom <u20594055@tuks.co.za>
2023-11-03 20:15:39 +0200

    fux

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -694,7 +694,7 @@ wiringpi.digitalWrite(LMot_Pin, 1)
 
 
 print("MC started")
-#motorCalibrate()
+motorCalibrate()
 #timeOnL, timeOnR, timeOffL, timeOffR = readCalibration()
 timeOnL=0.0071
 timeOnR=0.008
@@ -704,16 +704,16 @@ timeOffR=0.002
 #testDistances()
 #testAngles()
 
-angle,distance = readInstructions()
+#angle,distance = readInstructions()
 #timeOnL, timeOnR, timeOffL, timeOffR = readCalibration()
 # print("MC: time Left = ",timeOnL,"s ",timeOffL,"s")
 # print("MC: time Right = ",timeOnR,"s ",timeOffR,"s")
 
 
-angle,distance = motorControl_wThread(angle,distance)
-print("MC: Angle turned = ",angle*180/math.pi)
-print("MC: distance moved = ",distance)
-writeOdometry(angle,distance)
+# angle,distance = motorControl_wThread(angle,distance)
+# print("MC: Angle turned = ",angle*180/math.pi)
+# print("MC: distance moved = ",distance)
+# writeOdometry(angle,distance)
 
 
 # ##Actual code to do things
