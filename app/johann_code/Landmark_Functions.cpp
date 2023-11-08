f03f9b9632db228975fdb9c2e52d27c3af219fc8
Johann Strydom <u20594055@tuks.co.za>
2023-11-08 22:11:26 +0200

    fix

--- app/johann_code/Landmark_Functions.cpp
+++ app/johann_code/Landmark_Functions.cpp
@@ -43,7 +43,7 @@ namespace Landmark_Functions{
 
         cout<<"B x size = "<<x.size()<<endl;
         int sample_size = 80;
-        int max_iters= 600;
+        int max_iters= 900;
         float inlier_thresh=0.002; //0.0005
         int min_inlier = 5; // 5
         float angleThreshold = 20.0 * M_PI / 180.0;
