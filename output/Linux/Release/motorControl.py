7bbb9579bce93391757f0eda7ab98c9298006799
Johann Strydom <u20594055@tuks.co.za>
2023-11-09 21:01:37 +0200

    fix

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -954,9 +954,9 @@ angle,distance = readInstructions()
 
 # angle = -1*math.pi/2
 # distance = 0
-angle = math.pi/2
-#angle = 0
-distance = 200
+# angle = math.pi/2
+# #angle = 0
+# distance = 200
 
 
 if(distance > 900):
