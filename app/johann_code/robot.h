9350fbbcfb4f1626a27fe3ecf5ef17e0e81dd191
Johann Strydom <u20594055@tuks.co.za>
2023-11-12 10:41:04 +0200

    fix

--- app/johann_code/robot.h
+++ app/johann_code/robot.h
@@ -44,9 +44,9 @@ using namespace sl;
 // #define grid_ystep 400
 // #define grid_xstep 400
 
-extern grid_ystep
-extern grid_xstep
-extern boundThresh//If distance between gridPoint and lidarPoint <= Xmm then return false 
+extern float grid_ystep;
+extern float grid_xstep;
+extern float boundThresh;//If distance between gridPoint and lidarPoint <= Xmm then return false 
 
 struct PolPoint {
     double angle;
