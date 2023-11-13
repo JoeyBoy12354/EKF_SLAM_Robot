fa290e313405f78f646f7a7e1a0b37aa313a720c
Johann Strydom <u20594055@tuks.co.za>
2023-11-13 10:51:17 +0200

    fix

--- app/johann_code/Data_Functions.cpp
+++ app/johann_code/Data_Functions.cpp
@@ -8,7 +8,7 @@ using namespace Mapping_Functions;
 
 namespace Data_Functions{
 
-    int noCorners = 0;//If distance between gridPoint and lidarPoint <= Xmm then return false 
+    //int noCorners = 0;//If distance between gridPoint and lidarPoint <= Xmm then return false 
 
 
     bool compareByAngle(const PolPoint& a, const PolPoint& b) {
