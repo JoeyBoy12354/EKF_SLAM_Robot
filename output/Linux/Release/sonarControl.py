6eb61b262dc54d6d240c6b4f0eba4deeeddca47a
Johann Strydom <u20594055@tuks.co.za>
2023-11-13 15:47:23 +0200

    fix

--- output/Linux/Release/sonarControl.py
+++ output/Linux/Release/sonarControl.py
@@ -169,20 +169,20 @@ wiringpi.pinMode(echoPin1, 0)       # Set pin to 0 ( INPUT )
 wiringpi.pinMode(trigPin2, 1)       # Set pin to 1 ( OUTPUT )
 wiringpi.pinMode(echoPin2, 0)       # Set pin to 0 ( INPUT )
 
-wiringpi.pinMode(powerPin3, 1)       # Set pin to 1 ( OUTPUT )
-wiringpi.pinMode(trigPin3, 1)       # Set pin to 1 ( OUTPUT )
-wiringpi.pinMode(echoPin3, 0)       # Set pin to 0 ( INPUT )
-wiringpi.digitalWrite(powerPin3, 0) #Set high
+# wiringpi.pinMode(powerPin3, 1)       # Set pin to 1 ( OUTPUT )
+# wiringpi.pinMode(trigPin3, 1)       # Set pin to 1 ( OUTPUT )
+# wiringpi.pinMode(echoPin3, 0)       # Set pin to 0 ( INPUT )
+# wiringpi.digitalWrite(powerPin3, 0) #Set high
 
-while(True):
-    print("\n New RUN")
-    runSonarEdge()
-    time.sleep(1)
-    # print("Set High")
-    # wiringpi.digitalWrite(powerPin3, 0) #Set high
-    # time.sleep(3)
-    # print("Set Low")
-    # wiringpi.digitalWrite(powerPin3, 1) #Set high
-    # time.sleep(3)
+# while(True):
+#     print("\n New RUN")
+#     runSonarEdge()
+#     time.sleep(1)
+#     # print("Set High")
+#     # wiringpi.digitalWrite(powerPin3, 0) #Set high
+#     # time.sleep(3)
+#     # print("Set Low")
+#     # wiringpi.digitalWrite(powerPin3, 1) #Set high
+#     # time.sleep(3)
     
 #runSonar()
