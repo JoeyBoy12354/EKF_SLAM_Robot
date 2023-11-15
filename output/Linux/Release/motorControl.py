99dc0cba1e6145b873a51a50a0b1023591699dec
Johann Strydom <u20594055@tuks.co.za>
2023-11-15 11:14:47 +0200

    f
    undefined

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -1212,10 +1212,10 @@ print("MC started")
 
 
 
-    timeOnL=0.007250561797752809
-    timeOnR=0.008
-    timeOffL=0.002749438202247191
-    timeOffR=0.002
+timeOnL=0.007250561797752809
+timeOnR=0.008
+timeOffL=0.002749438202247191
+timeOffR=0.002
 
 
 
