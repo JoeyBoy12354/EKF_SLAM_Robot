6d6992aaa8f6c4f2329ad014d929e37f9603f914
Johann Strydom <u20594055@tuks.co.za>
2023-11-15 12:33:09 +0200

    fix
    undefined

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -1159,7 +1159,7 @@ wiringpi.digitalWrite(LMot_Pin, 1)
 
 
 print("MC started")
-motorCalibrate()
+#motorCalibrate()
 #timeOnL, timeOnR, timeOffL, timeOffR = readCalibration()
 # timeOnL=0.0071
 # timeOnR=0.008
