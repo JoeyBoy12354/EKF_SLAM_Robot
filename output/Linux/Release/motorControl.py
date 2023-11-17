a6a3d1d13b8c2751fddb3b4db7e256c2939f661d
Johann Strydom <u20594055@tuks.co.za>
2023-11-17 07:25:24 +0200

    f

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -941,10 +941,10 @@ def motorCalibrate():
 
 
 
-    timeOnL=0.007225561797752809
-    timeOnR=0.008
-    timeOffL=0.002774438202247191
-    timeOffR=0.002
+    # timeOnL=0.007225561797752809
+    # timeOnR=0.008
+    # timeOffL=0.002774438202247191
+    # timeOffR=0.002
 
 
 
@@ -972,10 +972,10 @@ def motorCalibrate():
     # timeOffR=0.002
 
 
-    timeOnL=0.006500561797752809
-    timeOnR=0.008
-    timeOffL=0.003499438202247191
-    timeOffR=0.002
+    # timeOnL=0.006500561797752809
+    # timeOnR=0.008
+    # timeOffL=0.003499438202247191
+    # timeOffR=0.002
 
     # timeOnL=0.006900561797752809
     # timeOnR=0.008
