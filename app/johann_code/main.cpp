3b18e846655f67f9949433063586c4bedf4d0d34
Johann Strydom <u20594055@tuks.co.za>
2023-11-17 22:43:41 +0200

    ok

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -1189,6 +1189,17 @@ void fullRunClean(ExtendedKalmanFilter& ekf,bool& mapped, bool& home, bool first
         //Run EKF
         ekf.runEKF();
 
+
+
+        if(ekf.State(11) != 0 && ekf.State(12)!=0){
+            cout<<"\n5th Corner was added assume this is problematic and will be solved with thread tests"<<endl;
+            cout<<"\n MAIN: b4_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
+            vector<float> accuracy;
+            ekf = runThread(ekf_old, lidarDataPoints,accuracy,  carPoints, polarCornerPoints);
+            cout<<"\n MAIN: afta_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
+
+        }
+
         cout<<"\n MAIN: after_ekf State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
         for(int i =3;i<dim;i=i+2){
             if(ekf.State[i] != 0 && ekf.State[i+1] != 0){
