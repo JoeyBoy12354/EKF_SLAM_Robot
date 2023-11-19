7d24309dd607d5a41cff94df9893741a8635e4c4
Johann Strydom <u20594055@tuks.co.za>
2023-11-19 16:09:09 +0200

    ff

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -1101,14 +1101,17 @@ void fullRun2(ExtendedKalmanFilter& ekf,bool& mapped, bool& home, bool firstRun,
 
         bool second = false;
         cout<<"\n MAIN: b4_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
-        float accuracy;
-        ekf = runThread(ekf_old, lidarDataPoints,accuracy,  carPoints, polarCornerPoints,second);
-        cout<<"Accuracy = "<<accuracy<<endl;
-        cout<<"\n MAIN: afta_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
+        float accuracy = 0;
+
+        if(firstRun == false){
+            ekf = runThread(ekf_old, lidarDataPoints,accuracy,  carPoints, polarCornerPoints,second);
+            cout<<"Accuracy = "<<accuracy<<endl;
+            cout<<"\n MAIN: afta_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
+        }
 
         
         //5th Corner thread fix
-        if((ekf.State(11) != 0 && ekf.State(12)!=0) || (noCorners == 1 && ekf.noNewCorners>0) || accuracy > 300){
+        if(( (ekf.State(11) != 0 && ekf.State(12)!=0) || (noCorners == 1 && ekf.noNewCorners>0) || accuracy > 300) && firstRun == false) {
             cout<<"\n5th Corner was added OR noCorners = 1 OR Acc>300 AGAIN assume this is problematic and will be solved with thread tests"<<endl;
             cout<<"\n MAIN: b4_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
 
@@ -1237,7 +1240,7 @@ void fullRunClean(ExtendedKalmanFilter& ekf,bool& mapped, bool& home, bool first
 
 
 
-        if(ekf.State(11) != 0 && ekf.State(12)!=0){
+        if((ekf.State(11) != 0 && ekf.State(12)!=0) && firstRun == false){
             bool second = false;
             cout<<"\n5th Corner was added assume this is problematic and will be solved with thread tests"<<endl;
             cout<<"\n MAIN: b4_thread State: x="<<ekf.State[0]<<", y="<<ekf.State[1]<<", w="<<ekf.State[2]*180/PI<<" deg"<<endl;
