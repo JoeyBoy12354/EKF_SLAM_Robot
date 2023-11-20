2d8529481410b2eedfc180ece4c5afb23eab74ff
Johann Strydom <u20594055@tuks.co.za>
2023-11-20 15:43:03 +0200

    s

--- app/johann_code/main.cpp
+++ app/johann_code/main.cpp
@@ -747,7 +747,7 @@ void atSim(){
 float scanAccuracy(vector<CarPoint> oldmap,vector<CarPoint> lidardata,Matrix<float, dim, 1> State, float& scanAcc){
     //Fit new scan
     //cout<<"ScanAcc pre fit"<<endl;
-    cout<<scanAcc<<<" Prefit for lidardata = "<<lidardata.size()<<" && State = "<<State(0)<<","<<State(1)<<","<<State(2)<<","<<endl;
+    cout<<scanAcc<<" Prefit for lidardata = "<<lidardata.size()<<" && State = "<<State(0)<<","<<State(1)<<","<<State(2)<<","<<endl;
     fitCartesian(lidardata,State(0),State(1),State(2));
     //cout<<"ScanAcc post fit"<<endl;
     cout<<"scanAcc postfit pre loop = "<<scanAcc<<" oldmap = "<<oldmap.size()<<endl;
