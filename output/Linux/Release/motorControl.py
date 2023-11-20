449cf719dbec396d8a88754a9fac5038409737a9
Johann Strydom <u20594055@tuks.co.za>
2023-11-20 13:29:55 +0200

    s

--- output/Linux/Release/motorControl.py
+++ output/Linux/Release/motorControl.py
@@ -1519,7 +1519,7 @@ wiringpi.digitalWrite(LMot_Pin, 1)
 
 
 print("MC started")
-#motorCalibrate()
+motorCalibrate()
 #timeOnL, timeOnR, timeOffL, timeOffR = readCalibration()
 # timeOnL=0.0071
 # timeOnR=0.008
@@ -1688,37 +1688,37 @@ timeOffR=0.002719438202247191
 # # print("MC: time Left = ",timeOnL,"s ",timeOffL,"s")
 # # print("MC: time Right = ",timeOnR,"s ",timeOffR,"s")
 
-angle,distance = readInstructions()
+# angle,distance = readInstructions()
 
-# angle = -1*math.pi/2
-# distance = 0
-# angle = math.pi/2
-# #angle = 0
-# distance = 200
+# # angle = -1*math.pi/2
+# # distance = 0
+# # angle = math.pi/2
+# # #angle = 0
+# # distance = 200
 
-# angle = math.pi/2
-# distance =0
+# # angle = math.pi/2
+# # distance =0
 
-# LNoRot,RNoRot  = speedControl(angle,0,True)
-
-
-# angle = getAngle(LNoRot,RNoRot,angle)
-# print("angle = ",angle*180/math.pi)
+# # LNoRot,RNoRot  = speedControl(angle,0,True)
 
 
-if(distance > 600):
-    print(" !! Resetting distance, ",distance," to 900mm")
-    distance = 900
-
-#Edge sonar
-wiringpi.pinMode(trigPin3, 1)       # Set pin to 1 ( OUTPUT )
-wiringpi.pinMode(echoPin3, 0)       # Set pin to 0 ( INPUT )
+# # angle = getAngle(LNoRot,RNoRot,angle)
+# # print("angle = ",angle*180/math.pi)
 
 
-angle,distance = motorControl_wThread(angle,distance)
-print("MC: Angle turned = ",angle*180/math.pi)
-print("MC: distance moved = ",distance)
-writeOdometry(angle,distance)
+# if(distance > 600):
+#     print(" !! Resetting distance, ",distance," to 900mm")
+#     distance = 900
+
+# #Edge sonar
+# wiringpi.pinMode(trigPin3, 1)       # Set pin to 1 ( OUTPUT )
+# wiringpi.pinMode(echoPin3, 0)       # Set pin to 0 ( INPUT )
+
+
+# angle,distance = motorControl_wThread(angle,distance)
+# print("MC: Angle turned = ",angle*180/math.pi)
+# print("MC: distance moved = ",distance)
+# writeOdometry(angle,distance)
 
 
 
